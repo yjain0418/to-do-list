59488b18326568c329115f15c3e667ab013b52f9
yjain0418 <12212162@nitkkr.ac.in>
2024-12-21 22:09:47 +0530

    Changes done

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ mongoose
 app.use(express.urlencoded({extended : true}))
 app.use(express.static("public"))
 app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "views"));
 
 
 //route
